bc5eaed674d0413351ffc9d971532f3d4cc8c9a9
Asraf Ali <asraf.cse@gmail.com>
2022-10-24 16:20:14 +0530

    chore: webpack configured

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, "/dist"),
         filename: "bundle.js",
-        library: 'CalculationBundle',
+        library: 'TimeComplexityAnalysis',
     },
     resolve: {
         extensions: [".tsx", ".ts", ".js", ".json"]
